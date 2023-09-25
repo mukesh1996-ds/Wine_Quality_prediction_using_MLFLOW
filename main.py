4665a13a1f59a583a8418852501172a9ea6904d3
mukesh1996-ds <mks.mukesh1996@gmail.com>
2023-09-25 12:55:50 +0530

    data validation done

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from quality_prediction_mlflow import logger
 from quality_prediction_mlflow.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from quality_prediction_mlflow.pipeline.stage_02_data_validation import DataVaildationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
 try:
     logger.info(f">>>>>>>stage {STAGE_NAME} started <<<<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
     logger.info(f">>>>>>>stage {STAGE_NAME} Completed <<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+try: 
+    logger.info(f">>>>>> Stage {STAGE_NAME} Started <<<<<<")
+    data_validation = DataVaildationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>> Stage {STAGE_NAME} Completed <<<<<<")
 except Exception as e:
     logger.exception(e)
     raise e
